98fc8c7817dd7ff0254891a9b5028205b6b73bde
ViTVetal <vvk1992@ukr.net>
2013-06-13 15:16:48 +0300

    Update Pie.java

--- src/com/example/checkwebsite/Pie.java
+++ src/com/example/checkwebsite/Pie.java
@@ -18,10 +18,10 @@ import android.widget.TextView;
 public class Pie extends Activity {
 	
 	public static final String TYPE = "type";
-	private static int[] COLORS = new int[] { Color.GREEN, Color.BLUE, Color.MAGENTA, Color.CYAN };//����� �����
-	private CategorySeries mSeries = new CategorySeries("");//��������� �� ��������
-	private DefaultRenderer mRenderer = new DefaultRenderer();//������� ������
-	private GraphicalView mChartView;//���� (�����) ������������� ���������
+	private static int[] COLORS = new int[] { Color.GREEN, Color.BLUE, Color.MAGENTA, Color.CYAN };
+	private CategorySeries mSeries = new CategorySeries("");
+	private DefaultRenderer mRenderer = new DefaultRenderer();
+	private GraphicalView mChartView;
 	TextView textOk;
 	TextView textError;
 	
@@ -39,7 +39,7 @@ public class Pie extends Activity {
 	}
 	
 	public int error_status_count(){
-		  dbHelper = new DBHelper(this);
+	      dbHelper = new DBHelper(this);
 	      String str = "";
 	      SQLiteDatabase db = dbHelper.getWritableDatabase();
 	      Cursor c = db.rawQuery("SELECT COUNT(*) FROM response WHERE answer != 200", null);
@@ -50,22 +50,22 @@ public class Pie extends Activity {
 	      return count_error;
 	}
 	public void add_pie_slice(){	   
-	        mSeries.add("Ok", ok_status_count());//��������� ����� ���������(����� + ��������)
-	        SimpleSeriesRenderer renderer = new SimpleSeriesRenderer();//������ �����
-	        renderer.setColor(Color.GREEN);//����������� ����� ���� �� �������
-	        mRenderer.addSeriesRenderer(renderer);//��������� �������� ������ �����
+	        mSeries.add("Ok", ok_status_count());
+	        SimpleSeriesRenderer renderer = new SimpleSeriesRenderer();
+	        renderer.setColor(Color.GREEN);
+	        mRenderer.addSeriesRenderer(renderer);
 	        
 	        if (mChartView != null) {
-	          mChartView.repaint();//���� �� �����, �� ������������ (���� ���� �����, �� ����� � �� ���� ��������)
+	          	mChartView.repaint();
 	        }
-	        SimpleSeriesRenderer renderer1 = new SimpleSeriesRenderer();//������ �����
-	        mSeries.add("Error", error_status_count());//��������� ����� ���������(����� + ��������)
+	        SimpleSeriesRenderer renderer1 = new SimpleSeriesRenderer();
+	        mSeries.add("Error", error_status_count());
 	      
-	        renderer1.setColor(Color.RED);//����������� ����� ���� �� �������
-	        mRenderer.addSeriesRenderer(renderer1);//��������� �������� ������ �����       
+	        renderer1.setColor(Color.RED);
+	        mRenderer.addSeriesRenderer(renderer1);
 	
 	        if (mChartView != null) {
-	          mChartView.repaint();//���� �� �����, �� ������������ (���� ���� �����, �� ����� � �� ���� ��������)
+	          	mChartView.repaint();
 	        }
 	}
 	
@@ -76,7 +76,7 @@ public class Pie extends Activity {
 	   
 	    textOk = (TextView) findViewById(R.id.textOk);
 	    textError = (TextView) findViewById(R.id.textError);
-	    //��������� �������
+	    
 	    mRenderer.setApplyBackgroundColor(true);
 	    mRenderer.setBackgroundColor(Color.argb(100, 245, 245, 245));
 	    mRenderer.setLabelsTextSize(16);
@@ -84,24 +84,23 @@ public class Pie extends Activity {
 	    mRenderer.setShowLegend(false);
 	    mRenderer.setMargins(new int[] { 20, 30, 15, 0 });
 	    mRenderer.setStartAngle(90);
-	    //���������� ������
-
+	   
 	    add_pie_slice();
 	}
 
-  @Override
-  protected void onResume() {
-    super.onResume();
+  	@Override
+ 	 protected void onResume() {
+   		super.onResume();
     
-    if (mChartView == null) {
-      LinearLayout layout = (LinearLayout) findViewById(R.id.chart);//������ � ������� ����� ��������
-      mChartView = ChartFactory.getPieChartView(this, mSeries, mRenderer);//�������� ���-���������
-      mRenderer.setClickEnabled(true);
-      mRenderer.setSelectableBuffer(1);
-
-      layout.addView(mChartView, new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
-    } else {
-      mChartView.repaint();
-    }
-  }
+	    if (mChartView == null) {
+	      LinearLayout layout = (LinearLayout) findViewById(R.id.chart);
+	      mChartView = ChartFactory.getPieChartView(this, mSeries, mRenderer);
+	      mRenderer.setClickEnabled(true);
+	      mRenderer.setSelectableBuffer(1);
+	
+	      layout.addView(mChartView, new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
+	    } else {
+	      mChartView.repaint();
+	    }
+	}
 }
